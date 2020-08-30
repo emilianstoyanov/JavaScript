327daf686ee45b691aa7a46aec483604ed54891d
Emilian Stoyanov <68276889+EmilianStoyanov@users.noreply.github.com>
2020-08-30 08:29:46 +0300

    Add files via upload

---
+++ basicsProjects/03.depositCalculator.js
@@ -0,0 +1,15 @@
+function depositCalculator(x, y, z) {
+    let depositedAmount = Number(x);
+    let depositTermMonths = Number(y);
+    let annualInterestRate = Number(z);
+
+    let calculateInterest = depositedAmount * annualInterestRate / 100;
+    let calculateInterestMonths = calculateInterest / 12;
+    let total = depositedAmount + (depositTermMonths * calculateInterestMonths);
+
+    console.log(total);
+
+
+}
+
+depositCalculator("200", "3", "5.7")
