20cd77f199cf615eb6af565340b3acc3e2a41fc6
Emilian Stoyanov <68276889+EmilianStoyanov@users.noreply.github.com>
2020-09-05 08:31:36 +0300

    Add files via upload

---
+++ basicsProjects/01.firstStepsInCodingLab/08.petShop.js
@@ -0,0 +1,9 @@
+function petShop(countDog, countOthers) {
+    let dogPrice = countDog * 2.50;
+    let otherPrice = countOthers * 4;
+    let total = dogPrice + otherPrice;
+    console.log(`${total} lv.`);
+
+}
+
+petShop(5, 4)
