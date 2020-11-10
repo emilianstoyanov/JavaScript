c69ca8bd9fb544138abf26f765f712eaf2bb1444
Emilian Stoyanov <68276889+EmilianStoyanov@users.noreply.github.com>
2020-11-10 23:41:12 +0200

    Add files via upload

---
+++ basicsProjects/03.conditionalStatementsLab/05.passwordGuess.js
@@ -0,0 +1,13 @@
+function passwordGuess(a) {
+    let searchPassword = "s3cr3t!P@ssw0rd"
+
+
+    if (a === searchPassword) {
+        console.log("Welcome")
+    } else {
+        console.log("Wrong password!")
+    }
+
+}
+
+passwordGuess("s3cr3@ssw0rd")
