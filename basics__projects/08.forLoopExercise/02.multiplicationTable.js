8a9deb010fe9c71a871c300f3d0390e94e54568e
EmilianStoyanov <EmilianStoyanov@github.com>
2020-11-22 22:01:36 +0200

    new task

---
+++ basics__projects/08.forLoopExercise/02.multiplicationTable.js
@@ -0,0 +1,13 @@
+function multiplicationTale(a) {
+    let num = Number(a);
+
+
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${i} * ${num} = ${i * num}`);
+
+    }
+
+}
+
+
+multiplicationTale("5")
