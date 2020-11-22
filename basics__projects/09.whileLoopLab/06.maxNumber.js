e6c803f7ae23def5c84f13f6e9c2668c73f406ba
EmilianStoyanov <EmilianStoyanov@github.com>
2020-11-22 20:19:54 +0200

    upload new task

---
+++ basics__projects/09.whileLoopLab/06.maxNumber.js
@@ -0,0 +1,29 @@
+function maxNumber(a) {
+
+    let save = -9999;
+    let index = 0;
+
+    while (true) {
+        let commandNum = a[index];
+        index++;
+
+        if (commandNum === "Stop"){
+            console.log(save);
+            break;
+        } else {
+            if (Number(commandNum) > save){
+                save = Number(commandNum);
+            }
+
+        }
+
+
+    }
+
+}
+
+maxNumber(["45",
+    "-20",
+    "7",
+    "99",
+    "Stop"])
