b977e85cbd1de2dc66ccb76b4363bca907c5672b
EmilianStoyanov <emilian.stoyanov@outlook.com>
2020-11-23 11:18:44 +0200

    new task

---
+++ basics__projects/07.forLoopLab/03.numbersOne...NwithStep3.js
@@ -0,0 +1,12 @@
+function numbersStep3(a) {
+    let num = Number(a);
+    let start = 1
+
+    for (start; start <= num; start += 3) {
+        console.log(start);
+
+    }
+
+}
+
+numbersStep3("15")
